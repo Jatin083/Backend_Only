0f351b57d40780b5d61503e296f4f44d61dca8e7
jatin <125881431+Jatin083@users.noreply.github.com>
2024-05-14 21:57:17 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+// install --> npm i packagename 
+// delete --> npm uninstall packagename
+
+// node_modules ye humesha aatey h jab bhi hum koi dependencies install
+// krte h agar ek express package h j kisi abc pe depend h to node_module usko bhi 
+// download kr leta hai 
+
